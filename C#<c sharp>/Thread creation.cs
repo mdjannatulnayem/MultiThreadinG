688b6c0ea4cb540fc91f01e3b692dcf11b085ff4
Md. Jannatul Nayem <64035356+mdjannatulnayem@users.noreply.github.com>
2022-05-08 00:52:07 +0600

    Update Thread creation.cs

--- C#<c sharp>/Thread creation.cs
+++ C#<c sharp>/Thread creation.cs
@@ -1,2 +1,45 @@
+using System;
+using System.Threading;
 
-/*pending....!*/
+namespace Threading //can be anyThing!!!
+{
+    class Program
+    {
+        static void Main(string[] args) //main function aka entryPoint!
+        {
+            Thread mainThread = Thread.CurrentThread;
+            mainThread.Name = "Main";
+            Thread thread2 = new Thread(() => countDown(21));
+            Thread thread1 = new Thread(() => countUp(10));
+            thread1.Start();
+            thread2.Start();
+            Console.WriteLine($"{mainThread.Name} thread going to sleep for 5 seconds!");
+            Thread.Sleep(5000);
+            if(thread1.IsAlive){
+                thread1.Join();
+                Console.WriteLine("Thread 1 succesfully joined!");
+            }
+            if(thread2.IsAlive){
+                thread2.Join();
+                Console.WriteLine("Thread 2 succesfully joined!");
+            }
+            System.Console.WriteLine($"Thread 1 running: {thread1.IsAlive}");
+            System.Console.WriteLine($"Thread 2 running: {thread2.IsAlive}");
+            Console.WriteLine($">>{mainThread.Name} thread exited the program!");
+        }
+
+        static void countUp(int loopFor){
+            for(int i=1;i<=loopFor;i++){
+                Console.WriteLine($"counting up until {loopFor} : {i}");
+                Thread.Sleep(500);
+            }
+        }
+
+        static void countDown(int loopFor){
+            for(int i=loopFor;i>=1;i--){
+                Console.WriteLine($"counting down until 1 : {i}");
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
