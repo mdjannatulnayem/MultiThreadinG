992a88cc5427593a52ed2a8051c7b868ec00d94d
Md. Jannatul Nayem <64035356+mdjannatulnayem@users.noreply.github.com>
2022-05-07 12:35:50 +0600

    Create Lock_guard.cpp

---
+++ Cpp/Lock_guard.cpp
@@ -0,0 +1,35 @@
+#include <chrono>
+#include <thread>
+#include <iostream>
+#include <mutex>
+
+std::mutex mTX;
+
+typedef unsigned long long ull;
+
+ull buffer = 0;
+
+void task(const char *threadNumber,int loopFor){
+    std::lock_guard<std::mutex> lock(mTX);
+    for(int i=0;i<loopFor;i++){
+        std::cout << threadNumber << " " << ++buffer << std::endl;
+    }
+}
+
+int main()
+{
+    auto startTime = std::chrono::high_resolution_clock::now();
+    std::thread t1(task,"T0",10);
+    std::thread t2(task,"T1",21);
+    if(t1.joinable()){
+        t1.join();
+    }
+    if(t2.joinable()){
+        t2.join();
+    }
+    auto endTime = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
+    std::cout << "Sec: " << duration.count()/1000000 << std::endl;
+    std::cout << "Micro: " << duration.count()%1000000 << std::endl;
+    return 0;
+}
